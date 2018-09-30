c4febc1fb18767a80f689b6399dceeca4d22e9e9
anurag kunchamwar <anurag.kunchamwar1@gmail.com>
2018-09-29 20:10:41 -0400

    added a library

--- Assign2_Anurag/Program.cs
+++ Assign2_Anurag/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
 
-namespace Assign2_Anurag
+namespace Assignment_2
 {
-    class Program
+    public partial class StockList
     {
-        static void Main(string[] args)
+        //param   (StockList)listToMerge : second list to be merged 
+        //summary      : merge two different list into a single result list
+        //return       : merged list
+        //return type  : StockList
+        public StockList MergeList(StockList listToMerge)
         {
-            Console.WriteLine("Hello World!");
+            StockList resultList = new StockList();
+
+            // write your implementation here
+
+            return resultList;
+        }
+
+        //param        : NA
+        //summary      : finds the stock with most number of holdings
+        //return       : stock with most shares
+        //return type  : Stock
+        public Stock MostShares()
+        {
+            Stock mostShareStock = null;
+
+            // write your implementation here
+
+            return mostShareStock;
+        }
+
+        //param        : NA
+        //summary      : finds the number of nodes present in the list
+        //return       : length of list
+        //return type  : int
+        public int Length()
+        {
+            int length = 0;
+
+            // write your implementation here
+
+            return length;
         }
     }
 }
